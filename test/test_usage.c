ffec2f4241fe1577f0a43f8b00dd47c816d6613b
František Bráblík <frantisek.brablik@gmail.com>
2025-01-03 12:27:51 +0100

    Test(usage): More parameters

--- test/test_usage.c
+++ test/test_usage.c
@@ -10,15 +10,14 @@
 #include <stdio.h>          // snprintf
 #include <string.h>         // memcpy
 
-void test_param( const char* param )
+void test_param( const char* argv[], int exit_val )
 {
-    const char* argv[] = { PROG_PATH, param, "--", TEST_EXIT, "0", NULL };
     pid_t pid = fork_exec( argv[ 0 ], argv );
 
     int status;
     assert( waitpid( pid, &status, 0 ) > 0 );
     assert( WIFEXITED( status ) );
-    assert( WEXITSTATUS( status ) == 0 );
+    assert( WEXITSTATUS( status ) == exit_val );
 }
 
 void test_case_params()
@@ -26,10 +25,37 @@ void test_case_params()
     const char* params[] = { "-h", "-j", "-d", "-p", "-n" };
 
     for ( unsigned i = 0; i < SIZE( params ); i++ )
-        test_param( params[ i ] );
+    {
+        const char* argv[] = { PROG_PATH, params[ i ], "--", TEST_EXIT, "0", NULL };
+        test_param( argv, 0 );
+    }
+}
+
+void test_case_bad_params()
+{
+    const char* params[] = { "-G", NULL };
+
+    for ( unsigned i = 0; i < SIZE( params ); i++ )
+    {
+        const char* argv[] = { PROG_PATH, params[ i ], "--", TEST_EXIT, "0", NULL };
+        test_param( argv, 1 );
+    }
+}
+
+void test_case_bad_cmd()
+{
+    const char* argv[] = { PROG_PATH, "invalid cmd", NULL };
+    pid_t pid = fork_exec( argv[ 0 ], argv );
+
+    int status;
+    assert( waitpid( pid, &status, 0 ) > 0 );
+    assert( WIFEXITED( status ) );
+    assert( WEXITSTATUS( status ) == 200 );
 }
 
 int main()
 {
     test_case_params();
+    test_case_bad_params();
+    test_case_bad_cmd();
 }
